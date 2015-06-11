09eeb0515f5f0bf7408f711e01bef647d05d8455
SourabhBanerjee <sourabh.banerjee@47billion.com>
2015-06-11 14:21:16 +0530

    Initial Commit

---
+++ EVA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EVA
+//
+//  Created by SourabhBanerjee on 6/11/15.
+//  Copyright (c) 2015 47Billion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
